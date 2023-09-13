52b5aaf9a4b72a6de258ae5041f597dd85006b2c
Sneiden <131291500+sneidenfernandes@users.noreply.github.com>
2023-09-13 23:59:00 +0530

    Update Data_modification.sql

--- SQL Climber/Data_modification.sql
+++ SQL Climber/Data_modification.sql
@@ -13,3 +13,22 @@ DECLARE @Country TABLE
 INSERT INTO @Country VAlUES(1, 'China', 'Beijing');
 
 SELECT * FROM @Country
+
+/*
+    Insert the following records into the declared table @Country:
+    Id  Name            Capital
+    2   Japan           Tokyo
+    3   India           New Delhi
+    4   South Korea     Seoul
+*/
+DECLARE @Country TABLE
+(
+    Id INT PRIMARY KEY,
+    Name NVARCHAR(100),
+    Capital NVARCHAR(100)
+)
+INSERT INTO @Country (Id, Name, Capital) VALUES (1, 'China', 'Beijing')
+INSERT INTO @Country(Id, Name, Capital) VALUES(2, 'Japan', 'Tokyo');
+INSERT INTO @Country(Id, Name, Capital) VALUES(3, 'India', 'New Delhi');
+INSERT INTO @Country(Id, Name, Capital) VALUES(4, 'South Korea', 'Seoul');
+SELECT * FROM @Country
