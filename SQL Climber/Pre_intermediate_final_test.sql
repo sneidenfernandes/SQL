56729c798f5b539a64fdfeb9e86dacfd0e1aa789
Sneiden <131291500+sneidenfernandes@users.noreply.github.com>
2023-10-14 22:58:06 +0530

    Update Pre_intermediate_final_test.sql

--- SQL Climber/Pre_intermediate_final_test.sql
+++ SQL Climber/Pre_intermediate_final_test.sql
@@ -32,3 +32,34 @@ BEGIN
 END
 SELECT [FlightTime] = @FlightTime
 
+/*
+    There is declared the flight date.
+    Calculate how many years, months, and days have passed since this date till today.
+*/
+DECLARE @Flight DATE = '1927-12-31'
+DECLARE @Today DATE = GETDATE()
+DECLARE @Years INT = DATEDIFF(YEAR, @Flight, @Today)
+DECLARE @endMonth INT = DATEPART(MONTH, @Today)
+DECLARE @startMonth INT = DATEPART(MONTH, @Flight)
+DECLARE @startDays INT = DATEPART(DAY, @Flight)
+DECLARE @endDays INT = DATEPART(DAY, @Today)
+DECLARE @Months INT = @endMonth - @startMonth
+DECLARE @Days INT =  @endDays - @startDays
+
+IF @days < 0
+BEGIN
+    SET @months = @months - 1
+    SET @days = DATEDIFF(DAY, DATEADD(MONTH, @months, DATEADD(YEAR, @years, @Flight)), @Today)
+END
+   	
+IF @months < 0
+BEGIN
+    SET @years = @years - 1
+    SET @months = 12 + @months
+END
+
+DECLARE @Result NVARCHAR(169) = 'The first solo nonstop transatlantic flight was '+CAST(@Years AS NVARCHAR(169))+
+		' years, '+CAST(@Months AS NVARCHAR(169))+' months, and '+CAST(@days AS NVARCHAR(169))+' days ago.'
+        
+SELECT 'Spirit of St. Louis' = @Result;
+
