22ec650a2c0dafe705ecf35c2daacb818805cd3a
Sneiden <131291500+sneidenfernandes@users.noreply.github.com>
2023-11-09 21:29:41 +0530

    Update HackerRank.sql

--- HackerRank/HackerRank.sql
+++ HackerRank/HackerRank.sql
@@ -80,4 +80,15 @@ WHERE
     OR
     s.CITY LIKE '%u';
 
+/**
+Weather Observation Station 8
+**/
+
+SELECT DISTINCT CITY FROM STATION 
+WHERE 
+    (CITY LIKE '%a' OR CITY LIKE '%e' OR CITY LIKE '%i' OR CITY LIKE '%o' OR CITY LIKE '%u') 
+AND 
+    (CITY LIKE 'a%' OR CITY LIKE 'e%' OR CITY LIKE 'i%' OR CITY LIKE 'o%' OR CITY LIKE 'u%');
+
+
 
